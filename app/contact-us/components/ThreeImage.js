873e0ee829f93fee223c0a08d68a9ed0d2b3d368
shabeeb k <shabeebkaip@gmail.com>
2024-06-10 16:51:39 +0530

    images layout fix

--- app/contact-us/components/ThreeImage.js
+++ app/contact-us/components/ThreeImage.js
@@ -1,22 +1,38 @@
 import Image from 'next/image'
 import React from 'react'
 
-const Threeimage = ({ images , index }) => {
-    console.log(index, "images")
+const Threeimage = ({ images, index }) => {
+    let even = index % 2 === 0 ? true : false;
     return (
         <div className='flex flex-col gap-3'>
+            {
+                index > 0 && even ?
+                    <>
+                        <div className='flex justify-end h-full gap-3'>
+                            <Image width={1000} height={500} src={images[0]} alt='Image' className='w-[50%]  rounded-[23px] mb-[-3px]  object-cover' />
+                            <Image width={1000} height={500} src={images[1]} alt='Image' className='w-[50%]  rounded-[23px]  object-cover' />
+                        </div>
+                        <div className='flex justify-center h-full '>
+                            <Image width={1000} height={500} src={images[2]} alt='Image' className='w-[100%] rounded-[23px]   object-cover' />
 
-            <div className='flex justify-center h-full '>
-                <Image width={1000} height={500} src={images[0]} alt='Image' className='w-[100%] rounded-[23px]   object-cover' />
+                        </div>
+                    </> :
+                    <>
+                        <div className='flex justify-center h-full '>
+                            <Image width={1000} height={500} src={images[0]} alt='Image' className='w-[100%] rounded-[23px]   object-cover' />
 
-            </div>
-            <div className='flex justify-end h-full gap-3'>
-                <Image width={1000} height={500} src={images[1]} alt='Image' className='w-[50%]  rounded-[23px] mb-[-3px]  object-cover' />
-                <Image width={1000} height={500} src={images[2]} alt='Image' className='w-[50%]  rounded-[23px]  object-cover' />
-            </div>
+                        </div>
+                        <div className='flex justify-end h-full gap-3'>
+                            <Image width={1000} height={500} src={images[1]} alt='Image' className='w-[50%]  rounded-[23px] mb-[-3px]  object-cover' />
+                            <Image width={1000} height={500} src={images[2]} alt='Image' className='w-[50%]  rounded-[23px]  object-cover' />
+                        </div>
+                    </>
+
+            }
+
 
         </div>
-       
+
     )
 }
 
